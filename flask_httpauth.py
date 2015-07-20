e33fd386fab3cf1a13044b922cad74eb01b2ee30
Lincan Li <email@lincan.li>
2015-07-20 17:39:24 +0800

    bug fixed

--- flask_httpauth.py
+++ flask_httpauth.py
@@ -1,35 +1,29 @@
-"""
-    flask_httpauth
-    ==================
-    
-    This module provides Basic and Digest HTTP authentication for Flask routes.
-    
-    :copyright: (C) 2014 by Miguel Grinberg.
-    :license:   BSD, see LICENSE for more details.
-    """
+# Creator: Lincan
+# Date: 7/20/15 - 1:47 PM
+
+# authorship
+__author__ = "Lincan"
 
 from functools import wraps
-from hashlib import md5
-from random import Random, SystemRandom
-from flask import request, make_response, session
+from flask import request, make_response
 
 
 class HTTPAuth(object):
     def __init__(self):
         def default_get_password(username):
             return None
-        
+
         def default_auth_error():
             return "Unauthorized Access"
-        
+
         self.realm = "Authentication Required"
         self.get_password(default_get_password)
         self.error_handler(default_auth_error)
-    
+
     def get_password(self, f):
         self.get_password_callback = f
         return f
-    
+
     def error_handler(self, f):
         @wraps(f)
         def decorated(*args, **kwargs):
@@ -42,22 +36,22 @@ class HTTPAuth(object):
             return res
         self.auth_error_callback = decorated
         return decorated
-    
+
     def login_required(self, f):
         @wraps(f)
         def decorated(*args, **kwargs):
             token = request.headers.get("X-TUSO-Application-Token")
-            
+
             # We need to ignore authentication headers for OPTIONS to avoid
             # unwanted interactions with CORS.
             # Chrome and Firefox issue a preflight OPTIONS request to check
             # Access-Control-* headers, and will fail if it returns 401.
             if request.method != 'OPTIONS':
-                if token and not self.authenticate(token):
+                if not token or not self.authenticate(token):
                     return self.auth_error_callback()
             return f(*args, **kwargs)
         return decorated
-    
+
     def username(self):
         if not request.authorization:
             return ""
@@ -69,24 +63,24 @@ class HTTPTokenAuth(HTTPAuth):
         super(HTTPTokenAuth, self).__init__()
         self.hash_password(None)
         self.verify_token(None)
-    
+
     def hash_password(self, f):
         self.hash_password_callback = f
         return f
-    
+
     def verify_token(self, f):
         self.verify_token_callback = f
         return f
-    
+
     def authenticate_header(self):
         return 'Basic realm="{0}"'.format(self.realm)
-    
+
     def authenticate(self, token):
         if not token:
             return False
-        
+
         if self.verify_token_callback:
             return self.verify_token_callback(token)
-        
+
         return False
 
